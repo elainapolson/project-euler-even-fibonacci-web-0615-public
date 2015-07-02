45e79bd4d541e7e9b0ae44791c3cf9fb0ddbd8e9
elainapolson <elainapolson@gmail.com>
2015-07-02 09:28:13 -0400

    done

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -9,16 +9,13 @@ class EvenFibonacci
   def find_fibonacci_numbers
     #make all the numbers an array
     array = [1, 2]
-    
     while (array[-1] + array[-2]) < @limit
       array << array[-1] + array[-2]
     end
     array
-    
   end
 
   def sum
-
     numbers = find_fibonacci_numbers
     sum = 0
     numbers.each do |n|
